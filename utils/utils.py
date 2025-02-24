9cfc085ee7ed0e501365e0f202471d775937ae9d
Le Hoang <lehoangnguyen510@gmail.com>
2025-02-24 17:01:01 +0100

    add evaluation

--- utils/utils.py
+++ utils/utils.py
@@ -15,13 +15,26 @@ def set_seed(seed: int = 555):
 	torch.backends.cudnn.benchmark = False
 
 def de_normalize(img, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
-    result = img*std+mean
-    result = np.clip(result, 0, 1)
-    return result
-    
+	result = img*std+mean
+	result = np.clip(result, 0, 1)
+	return result
+	
 def target_transform(target, img_size=(128, 128)):
-    img = transforms.Resize(img_size)(target)
-    img = transforms.functional.pil_to_tensor(img).squeeze()
-    img = img-1
-    img = img.to(torch.long)
-    return img
+	img = transforms.Resize(img_size)(target)
+	img = transforms.functional.pil_to_tensor(img).squeeze()
+	img = img-1
+	img = img.to(torch.long)
+	return img
+
+def evaluate(model, test_loader, criterion, device):
+	model.eval()
+	test_loss = 0.0
+	with torch.no_grad():
+		for inputs, labels in test_loader:
+			inputs, labels = inputs.to(device), labels.to(device)
+			outputs = model(inputs)
+			loss = criterion(outputs, labels)
+			test_loss += loss.item()
+	
+	test_loss = test_loss / len(test_loader)
+	return test_loss
